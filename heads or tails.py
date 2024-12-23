91c8edda871d118f7102100e8bc1e2d40818346a
kemi-oo <therealok8@gmail.com>
2024-12-24 00:02:54 +0100

    Add files via upload

---
+++ heads or tails.py
@@ -0,0 +1,22 @@
+import random
+
+from Tools.demo.mcast import MYTTL
+
+import My_Module
+
+#random_integer = random.randint(1,118)
+#print(random_integer)
+#print(My_Module.clone)
+
+#random_number_0_to_1 = random.random() * 10
+#print(random_number_0_to_1)
+
+#random_float = random.uniform(1,10)
+#print(random_float)
+
+
+random_heads_or_tails = random.randint(0,1)
+if random_heads_or_tails == 0:
+    print("Heads")
+else: print("Tails")
+
